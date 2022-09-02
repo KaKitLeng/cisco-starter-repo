5380eae391a675282e303b098d4efbf3e5c05e63
KaKitLeng <jasonlengkakit@gmail.com>
2022-09-01 22:06:14 -0400

    Update
    Display public ip with better CSS design

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,32 +1,36 @@
-import { useState } from 'react';
+//import { useState } from 'react';
 import './App.css';
 import Banner from './components/banner';
 import Card from './components/card';
-
+import AddressDisplay from './components/ipAddress';
 
 function App() {
-  const [ip6, setIp6] = useState('')
-  fetch('https://api64.ipify.org?format=json')
-    .then(results => results.json())
-    .then(data => setIp6(data.ip));
+//   const [ip6, setIp6] = useState('')
+//   fetch('https://api64.ipify.org?format=json')
+//     .then(results => results.json())
+//     .then(data => setIp6(data.ip));
 
-    const [ip4, setIp4] = useState('')
-    fetch('https://api.ipify.org?format=json')
-      .then(results => results.json())
-      .then(data => setIp4(data.ip));
+//     const [ip4, setIp4] = useState('')
+//     fetch('https://api.ipify.org?format=json')
+//       .then(results => results.json())
+//       .then(data => setIp4(data.ip));
     
   return (
     <>
       <Banner />
       <div className="wrapper">
-        <Card 
+        <Card
         // img = 'https://i.picsum.photos/id/744/200/300.jpg?hmac=QB_puLFiEKiOkiVJXMeY6ie3KJ4AgggamJiRa4kobOo'
-        // title = 'testing'
-        text = {ip6}/>
+        //text = 'Public IPv4'
+        name = 'Public IPv4'>
+            <AddressDisplay url = 'https://api.ipify.org?format=json' />
+        </Card>
         <Card 
         //img = 'https://i.picsum.photos/id/744/200/300.jpg?hmac=QB_puLFiEKiOkiVJXMeY6ie3KJ4AgggamJiRa4kobOo'
         //title = 'testing'
-        text = {ip4}/>
+        name = 'Public IPv6'>
+            <AddressDisplay url = 'https://api64.ipify.org?format=json' />
+        </Card>
       </div>
     </>
 
--- sextant/src/components/card.js
+++ sextant/src/components/card.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, {Component} from 'react'
 import './card.css'
 
-function Card(props) {
-  return (
-    <div className='card-container'>
-        {/* <div className='image-container'>
-            <img src={props.img} alt=''/>
-        </div> */}
-        {/* <div className="card-title">
-            {props.title}
-        </div> */}
-        <p className="card-text">
-            {props.text}
-        </p>
-    </div>
-  )
+// function Card(props) {
+//   return (
+//     <div className='card-container'>
+//         <h2 className="card-title">{props.title}</h2>
+//                 <div className="card-text">
+//                     {props.children}
+//                 </div>
+//     </div>
+//   )
+// }
+
+class Card extends Component {
+  render() {
+      return (
+          <div className="card-container">
+              <h2 className="card-title">{this.props.name}</h2>
+              <div className="card-text">
+                  {this.props.children}
+              </div>
+          </div>
+      );
+  }
 }
 
 export default Card
